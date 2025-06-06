69f5f2fb39b9560aa8016a6c7f35719b61d1f615
Alejopl6 <alejopl6@outlook.es>
2025-06-06 12:28:30 -0500

    refactor: update Timeline component layout and styles for improved readability and visual appeal

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -37,13 +37,15 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       className="w-full font-sans md:px-10"
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-        Un camino claro para que evoluciones, no solo mejores
-        </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-        Nuestro proceso está diseñado para guiar tu transformación, no solo mejorar tu desempeño.
-        </p>
+      <div className="mx-auto flex max-w-container flex-col items-center gap-4 text-center sm:gap-16">
+        <div className="flex flex-col items-center gap-4 px-4 sm:gap-8">
+          <h2 className="max-w-[720px] text-3xl font-semibold leading-tight sm:text-5xl sm:leading-tight">
+            Un camino claro para que evoluciones, no solo mejores
+          </h2>
+          <p className="text-md max-w-[600px] font-medium text-muted-foreground sm:text-xl">
+            Nuestro proceso está diseñado para guiar tu transformación, no solo mejorar tu desempeño.
+          </p>
+        </div>
       </div>
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
